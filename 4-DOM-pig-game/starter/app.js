23f75987931ce48a5affe6b3c886f9c17c63e361
chenyangufl <chenyang1992hi@gmail.com>
2018-09-05 22:53:41 -0700

    update code

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,4 +9,18 @@ GAME RULES:
 
 */
 
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x = document.querySelector('#score-0').textContent;
+document.querySelector('.dice').style.display = 'none';
+
 
