f2a8a6c9edf845f4a0b6abe12ec260d4e406c996
chenyangufl <chenyang1992hi@gmail.com>
2018-09-11 21:12:51 -0700

    lecture 65

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,8 +1,80 @@
 // Function constructor
 
+/*
 var john = {
     name: 'John',
     yearOfBirth: 1990,
     job: 'teacher'
 };
 
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+        console.log(2016 - this.yearOfBirth);
+}
+
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+
+*/
+
+
+
+// Object.create
+
+/*
+
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth)
+    }    
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: { value: 'Jane' },
+    yearOfBirth: {value: 1990},
+    job: {value: 'designer'}
+});
+
+*/
+
+
+
+// Primitives vs objects
+
+var a =23;
+var b=a;
+a = 46;
+
+// Primitives, every time create a copy
+// objects, every time point to that object
+
+
+var years = [1990, 1992, 1994, 2000];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i=0; i< arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016-el;
+}
+
+console.log(arraycalc(years, calculateAge));
